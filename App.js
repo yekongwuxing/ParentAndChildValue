94e9c1b09970e1edb79cf84b8b3c808beba2a55b
jiangmm <jiangmm@jchvip.com>
2018-08-14 15:59:58 +0800

    FlatList中每个item封装传值

--- App.js
+++ App.js
@@ -22,20 +22,50 @@ export default class App extends Component{
       super(props);
       // 初始状态
       this.state = {
-          data:{}
+          data:[]
       };
     }
 
     componentDidMount() {
         this.setState({
-            data:{'name':'小名','age':'18','sex':'男','education':'高中'}
+            data:[{'name':'小名','age':'18','sex':'男','education':'高中'},
+                {'name':'小明','age':'18','sex':'男','education':'高中'},
+                {'name':'小花','age':'18','sex':'女','education':'高中'}]
         })
     }
+    _renderItem = ({item}) => {
+        return(
+            <RenderItem info={item}/>
+        )
+    }
+    _keyExtractor = (item,index) => {
+        return index
+    }
+
     render() {
-    return (
-      <RenderItem info={this.state.data}
-      />
+        return (
+            <FlatList style={{flex:1}}
+                      data={this.state.data}
+                      renderItem={this._renderItem}
+                      keyExtractor={this._keyExtractor}
+            />
+
     );
   }
 }
 
+const styles = StyleSheet.create({
+
+    container:{
+        marginTop: 20,
+        borderBottomWidth: 1,
+        borderColor: '#e5e5e5',
+        backgroundColor: 'white',
+    },
+    txt:{
+        fontSize:15,
+        paddingHorizontal:20,
+        color:'red'
+    }
+
+});
